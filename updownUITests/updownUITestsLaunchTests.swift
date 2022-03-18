fe33d3d2c728f368b0073346803106785645a45c
Willie Johnson <liwa.johnson@gmail.com>
2022-03-18 16:00:05 -0400

    Initial Commit

---
+++ updownUITests/updownUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  updownUITestsLaunchTests.swift
+//  updownUITests
+//
+//  Created by Willie Liwa Johnson on 3/18/22.
+//
+
+import XCTest
+
+class updownUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
